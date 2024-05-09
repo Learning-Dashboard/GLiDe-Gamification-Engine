9b057a6f6b159ac9591c8389b390938d1f65b80f
albert-minana <albert.minana@estudiantat.upc.edu>
2024-05-09 21:30:43 +0200

    Automatic documentation generation following OpenAPI Specification

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# GLiDe-Gamification-Engine
-RESTful API for GLiDe's Gamification Engine
+# GLiDe's Gamification Engine ![](https://img.shields.io/badge/License-Apache2.0-blue.svg)
+RESTful API for the GLiDe's Gamification Engine
+
+## Documentation
+
+### OpenAPI Specification
+The OpenAPI Specification for the GLiDe's Gamification Engine can be found in different formats:
+* __Swagger UI page in *HTML* format__ available at http://localhost:8080/swagger-ui.html
+* __Description in *json* format__ available at http://localhost:8080/v3/api-docs
+* __Description in *yaml* format__ available at http://localhost:8080/v3/api-docs.yaml
---
+++ src/main/java/edu/upc/gessi/glidegamificationengine/config/OpenAPIConfig.java
@@ -0,0 +1,21 @@
+package edu.upc.gessi.glidegamificationengine.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GLiDe's Gamification Engine")
+                        .version("1.0")
+                        .description("This is the OpenAPI Specification for the GLiDe's Gamification Engine")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
