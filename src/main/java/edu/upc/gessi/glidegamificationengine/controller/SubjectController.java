c38f9167dc021fed94468d2f8e750e53c512c7e1
SnappySM <nicola.scognamillo@estudiantat.upc.edu>
2024-12-09 18:13:05 +0100

    Operació per fer post d'un subject

--- src/main/java/edu/upc/gessi/glidegamificationengine/controller/SubjectController.java
+++ src/main/java/edu/upc/gessi/glidegamificationengine/controller/SubjectController.java
@@ -9,12 +9,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 @RestController
@@ -33,5 +33,20 @@ public class SubjectController {
     public ResponseEntity<List<SubjectDTO>> getSubjects() {
         List<SubjectDTO> subjectDtos = subjectService.getSubjects();
         return ResponseEntity.ok(subjectDtos);
+    }
+
+    @Operation(summary = "Create subject", description = "Create a new subject.", tags = { "subjects" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "CREATED: AchievementDTO object.", content = @Content(schema = @Schema(implementation = SubjectDTO.class))),
+            @ApiResponse(responseCode = "409", description = "CONFLICT: (1) SUbject acronym cannot be blank. (2) The given subject acronym is already used.", content = @Content)
+    })
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SubjectDTO> createSubject(@RequestPart(value = "acronym") String acronym,
+                                                    @RequestPart(value = "code") Integer code,
+                                                    @RequestPart(value = "name") String name,
+                                                    @RequestPart(value = "school") String school,
+                                                    @RequestPart(value = "studies") String studies){
+        SubjectDTO savedSubjectDto = subjectService.createSubject(acronym, code, name, school, studies);
+        return new ResponseEntity<>(savedSubjectDto, HttpStatus.CREATED);
     }
 }
