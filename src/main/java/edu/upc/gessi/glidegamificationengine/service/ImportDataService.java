084b53ba0d07ad67b9c367bea0725cbce06c5fd0
SnappySM <nicola.scognamillo@estudiantat.upc.edu>
2024-12-18 19:15:43 +0100

    Treure import innecessari

--- src/main/java/edu/upc/gessi/glidegamificationengine/service/ImportDataService.java
+++ src/main/java/edu/upc/gessi/glidegamificationengine/service/ImportDataService.java
@@ -3,7 +3,6 @@ package edu.upc.gessi.glidegamificationengine.service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public interface ImportDataService {
     void importData(String gameSubjectAcronym, Integer gameCourse, String gamePeriod, Integer groupNumber, MultipartFile importedData) throws IOException;
